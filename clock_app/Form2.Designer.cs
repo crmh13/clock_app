84825390ec9b2ea70ef6a33b72151aae9f7995dc
crmh <t260robo@gmail.com>
2022-08-27 10:59:09 +0900

    フォームの見た目を修正

--- clock_app/Form2.Designer.cs
+++ clock_app/Form2.Designer.cs
@@ -128,7 +128,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(610, 297);
+            this.ClientSize = new System.Drawing.Size(551, 297);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
